e699091fbd1446db4a63c51b56b0f31fe675c126
mad1na2010-arch <komilovam2010@gmail.com>
2025-12-02 15:16:54 +0300

    homework

---
+++ src/m.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return <h2>header</h2>
+}
+
+function Content() {
+    return <h2>content</h2>
+}
+
+function Footer() {
+    return <h2>footer</h2>
+}
+
+export {Header,Content,Footer};
